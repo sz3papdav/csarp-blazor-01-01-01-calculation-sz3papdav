a11cc898310109b889b7dc085855d67ee70f57e4
sz3papdav <sz3papdav@vasvari.org>
2023-11-29 12:37:04 +0100

    keszs

---
+++ Calculation/Calculation/Client/Models/Szamitas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using ViewModels.BaseClass;
+
+namespace Calculation.Client.Models
+{
+    public class Szamit : ViewModelBase
+    {
+        public double _egyikszam;
+        public double _masikszam;
+
+        public Szamit()
+        {
+            _egyikszam = 0;
+            _masikszam = 0;
+        }
+        public string Egyikszam
+        {
+            get => _egyikszam.ToString();
+            set => SetValue(ref _egyikszam, Convert.ToDouble(value));
+        }
+        public string Masikszam
+        {
+            get => _masikszam.ToString();
+            set => SetValue(ref _masikszam, Convert.ToDouble(value));
+        }
+
+        public double EredmenySzamitas
+        {
+            get
+            {
+                double ertek = _egyikszam * _masikszam;
+                return ertek;
+            }
+        }
+        public string Eredmeny
+        {
+            get
+            {
+                return "A 2 szám szorzata: " + EredmenySzamitas;
+            }
+        }
+        public void Compute()
+        {
+            OnPropertyChanged(nameof(EredmenySzamitas));
+            OnPropertyChanged(nameof(Eredmeny));
+        }
+    }
+}
